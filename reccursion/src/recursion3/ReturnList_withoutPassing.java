e41dd3ab7c41f7f4de6c8ccde53fa428396bc01c
abhijaydas09 <148853650+abhijaydas09@users.noreply.github.com>
2024-02-22 22:19:57 +0530

    Add files via upload

---
+++ reccursion/src/recursion3/ReturnList_withoutPassing.java
@@ -0,0 +1,11 @@
+package recursion3;
+
+// goal : return the list but don't take in argument.
+// we know return type will be array list.
+// challenges :every call will have a new list git ct
+public class ReturnList_withoutPassing {
+    public static void main(String[] args) {
+        int[] arr = {3,5,7,9,13} ;
+         
+    }
+}
